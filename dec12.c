13993fc00becd421ca2925603622268eb50e4550
Joachim <Joachim.e.nebel@gmail.com>
2022-12-12 14:10:51 +0100

    12. dec done

--- dec12.c
+++ dec12.c
@@ -4,13 +4,19 @@
 #include <ctype.h>
 #include <stdbool.h>
 
-void sol12() {
+bool can_move(int part, int curr_ele, int target_ele) {
+    if (part == 1) return target_ele <= curr_ele + 1;
+    else return target_ele >= curr_ele - 1;
+}
+
+int solve(int part) {
     char const* const fileName = "C:\\Users\\Joachim\\CLionProjects\\AdventOfCode\\input12.txt";
     FILE* file = fopen(fileName, "r");
 
     char line[120];
 
     int end_x = 0, end_y = 0;
+    int start_x = 0, start_y = 0;
     int width, height = 0;
 
     //Load size
@@ -20,20 +26,17 @@ void sol12() {
     }
 
     int map[width][height];
-    int dists[width][height];
-
-    printf("%d by %d\n", width, height);
 
     //Load map
     file = fopen(fileName, "r");
     for (int y = 0; y < height; ++y) {
         fgets(line, 120, file);
         for (int x = 0; x < width; ++x) {
-            dists[x][y] = -1;
             //start & end
             if (line[x] == 'S') {
+                start_x = x;
+                start_y = y;
                 map[x][y] = 'a';
-                dists[x][y] = 0;
                 continue;
             }
             if (line[x] == 'E') {
@@ -48,6 +51,15 @@ void sol12() {
         }
     }
 
+    int dists[width][height];
+    memset(dists, -1 , sizeof(dists));
+
+    if (part == 1) {
+        dists[start_x][start_y] = 0;
+    } else {
+        dists[end_x][end_y] = 0;
+    }
+
     //Breadth first
     int max_steps = 1000;
     for (int i = 0; i < max_steps; ++i) {
@@ -58,23 +70,26 @@ void sol12() {
         for (int x = 0; x < width; ++x) {
             for (int y = 0; y < height; ++y) {
                 if (dists[x][y] == -1) continue;
-
                 int curr_ele = map[x][y];
 
+                if (part == 2 && curr_ele == 'a') {
+                    return i;
+                }
+
                 //Right (x + 1)
-                if (x + 1 < width && dists[x + 1][y] == -1 && map[x + 1][y] <= curr_ele + 1) {
+                if (x + 1 < width && dists[x + 1][y] == -1 && can_move(part, curr_ele, map[x + 1][y])) {
                     new_dists[x + 1][y] = i + 1;
                 }
                 //Left (x - 1)
-                if (x > 0 && dists[x - 1][y] == -1 && map[x - 1][y] <= curr_ele + 1) {
+                if (x > 0 && dists[x - 1][y] == -1 && can_move(part, curr_ele, map[x - 1][y])) {
                     new_dists[x - 1][y] = i + 1;
                 }
                 //Down (y + 1)
-                if (y + 1 < height && dists[x][y + 1] == -1 && map[x][y + 1] <= curr_ele + 1) {
+                if (y + 1 < height && dists[x][y + 1] == -1 && can_move(part, curr_ele, map[x][y + 1])) {
                     new_dists[x][y + 1] = i + 1;
                 }
                 //Up (y - 1)
-                if (y > 0 && dists[x][y - 1] == -1 &&map[x][y - 1] <= curr_ele + 1) {
+                if (y > 0 && dists[x][y - 1] == -1 && can_move(part, curr_ele, map[x][y - 1])) {
                     new_dists[x][y - 1] = i + 1;
                 }
             }
@@ -82,20 +97,14 @@ void sol12() {
 
         memcpy(dists, new_dists, sizeof(new_dists));
 
-        if (dists[end_x][end_y] != -1) {
-            break;
+        if (part == 1 && dists[end_x][end_y] != -1) {
+            fclose(file);
+            return dists[end_x][end_y];
         }
     }
+}
 
-    /*for (int y = 0; y < height; ++y) {
-        for (int x = 0; x < width; ++x) {
-            printf(" %d ", dists[x][y]);
-        }
-        printf("\n");
-    }*/
-
-    printf("Part 1: %d\n", dists[end_x][end_y]);
-    printf("Part 2: \n");
-
-    fclose(file);
+void sol12() {
+    printf("Part 1: %d\n", solve(1));
+    printf("Part 2: %d\n", solve(2));
 }
